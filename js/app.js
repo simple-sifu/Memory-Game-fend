b1b46ffb1d7d751d078b21f7bdf00afe11c5a695
Tommy Han <tommy@Tommys-Air.home>
2020-05-05 15:06:53 -0400

    save these settings.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,17 @@
 // cards array holds all cards
 let card = document.getElementsByClassName('card');
-let cards = [...card];
+const cards = [...card];
 
 // deck of all cards in game
 const deck = document.getElementById('card-deck');
 
-// close icon in modal
-const closeicon = document.querySelector('.close');
-
-// declare modal
-const modal = document.getElementById('popup1');
-
 // array for opened cards
-const openedCards = [];
+// const openedCards = [];
 
 // @description function to start a new play
 function startGame() {
-  // shuffle deck
-  //   cards = shuffle(cards);
   // remove all exisiting classes from each card
+  console.log('startGame');
   for (let i = 0; i < cards.length; i++) {
     deck.innerHTML = '';
     [].forEach.call(cards, function(item) {
@@ -28,55 +21,20 @@ function startGame() {
   }
 }
 
-// @description shuffles cards when page is refreshed / loads
-document.body.onload = startGame();
-
 // @description toggles open and show class to display cards
-var displayCard = function() {
+const displayCard = function() {
+  console.log('displayCard');
   this.classList.toggle('open');
   this.classList.toggle('show');
   this.classList.toggle('disabled');
 };
 
-// @description add opened cards to OpenedCards list and check if cards are match or not
-function cardOpen() {
-  openedCards.push(this);
-  var len = openedCards.length;
-}
+// @description shuffles cards when page is refreshed / loads
+document.body.onload = startGame();
 
-// @description game timer
-// let second = 0;
-// let minute = 0;
-// let hour = 0;
-// let timer = document.querySelector('.timer');
-// var interval;
-// function startTimer() {
-//   interval = setInterval(function() {
-//     timer.innerHTML = minute + 'mins ' + second + 'secs';
-//     second++;
-//     if (second === 60) {
-//       minute++;
-//       second = 0;
-//     }
-//     if (minute === 60) {
-//       hour++;
-//       minute = 0;
-//     }
-//   }, 1000);
-// }
-
-// @description close icon on modal
-// function closeModal() {
-//   closeicon.addEventListener('click', function(e) {
-//     modal.classList.remove('show');
-//     startGame();
-//   });
-// }
-
-// loop to add event listeners to each card
+// loop to add displayCard listeners to each card
 for (let i = 0; i < cards.length; i++) {
+  console.log('add displayCard listener to each card');
   card = cards[i];
   card.addEventListener('click', displayCard);
-  card.addEventListener('click', cardOpen);
-  //   card.addEventListener('click', congratulations);
 }
